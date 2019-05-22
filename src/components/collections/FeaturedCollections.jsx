59c287412890fd63eb225e9cb26e74e648042402
Doris Warren <doriswarren11@gmail.com>
2019-05-21 21:32:13 -0700

    add photos - ass skeleton for freatured collections

---
+++ src/components/collections/FeaturedCollections.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function FeaturedCollections () {
+  return(
+    <main className="collectionsConatiner">
+
+      <style jsx>{`
+
+      `}</style>
+      <section>
+      
+      </section
+
+
+    </main>
+  )
+}
+
+export default FeaturedCollections;
